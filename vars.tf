16e727d0ff85b18d2f456d8f7103cab8c87d3750
Irfan Shaikh <irfan.shaikh@ibm.com>
2024-07-23 22:20:29 +0530

    terraform automation file added

---
+++ vars.tf
@@ -0,0 +1,17 @@
+variable "AWS_REGION" {
+  default = "us-east-1"
+}
+
+variable "AMIS" {
+  type = map(string)
+  default = {
+    us-east-1  = "ami-04a81a99f5ec58529"
+    eu-west-1  = "ami-0333305f9719618c7"
+    ap-south-1 = "ami-06984ea821ac0a879"
+  }
+}
+
+variable "subnet" {
+  default = "public"
+}
+
