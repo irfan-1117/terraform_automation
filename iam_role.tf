73828b3394fef2e74c93686d5764ae656abdda3d
Irfan Shaikh <irfan.shaikh@ibm.com>
2024-07-29 16:48:03 +0530

    adding python pip and awc cli installation in shell sript

--- iam_role.tf
+++ iam_role.tf
@@ -1,38 +1,43 @@
-data "aws_iam_policy" "ssmpolicy" {
-  name = "AmazonSSMManagedInstanceCore"
-}
+# Create IAM role
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2_role"
 
-resource "aws_iam_role" "ec2_ssm_roles" {
-  name = "ec2_ssm_roles"
-
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
+    Version = "2012-10-17",
     Statement = [
       {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
         Principal = {
           Service = "ec2.amazonaws.com"
         }
-      },
+      }
     ]
   })
-
-  tags = {
-    created = "terraform"
-  }
 }
 
+# Attach a policy to the role
+resource "aws_iam_role_policy" "ec2_policy" {
+  name = "ec2_policy"
+  role = aws_iam_role.ec2_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = [
+          "s3:*",
+          "ec2:*"
+        ],
+        Effect   = "Allow",
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+# Create IAM instance profile
 resource "aws_iam_instance_profile" "ec2_profile" {
-  name = "ec2_ssm_profile"
-  role = aws_iam_role.ec2_ssm_roles.name
-}
-
-resource "aws_iam_policy_attachment" "policy-attach" {
-  name       = "policy-attachment"
-  roles      = [aws_iam_role.ec2_ssm_roles.name]
-  policy_arn = data.aws_iam_policy.ssmpolicy.arn
+  name = "ec2_profile"
+  role = aws_iam_role.ec2_role.name
 }
