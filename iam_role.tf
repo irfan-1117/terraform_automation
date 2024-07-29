bc6dde32e28c77fcbbc60e8ae65c1698cc911b1d
Irfan Shaikh <irfan.shaikh@ibm.com>
2024-07-29 18:06:20 +0530

    adding python pip and awc cli installation in shell sript

--- iam_role.tf
+++ iam_role.tf
@@ -1,4 +1,4 @@
-# Create a combined IAM Role for EC2 with ECR, ECS, and Lambda S3 permissions
+# Create a combined IAM Role for EC2 with ECR, ECS, S3, EKS, and Lambda permissions
 resource "aws_iam_role" "combined_role" {
   name = "combined_role"
 
@@ -18,8 +18,8 @@ resource "aws_iam_role" "combined_role" {
 
 # Attach the combined policy to the role
 resource "aws_iam_role_policy" "combined_policy" {
-  name   = "combined_policy"
-  role   = aws_iam_role.combined_role.id
+  name = "combined_policy"
+  role = aws_iam_role.combined_role.id
 
   policy = jsonencode({
     Version = "2012-10-17",
@@ -27,52 +27,13 @@ resource "aws_iam_role_policy" "combined_policy" {
       {
         Effect = "Allow",
         Action = [
-          "ecr:GetDownloadUrlForLayer",
-          "ecr:BatchGetImage",
-          "ecr:BatchCheckLayerAvailability",
-          "ecr:PutImage",
-          "ecr:InitiateLayerUpload",
-          "ecr:UploadLayerPart",
-          "ecr:CompleteLayerUpload",
-          "ecr:DescribeRepositories",
-          "ecr:ListImages",
-          "ecr:CreateRepository",
-          "ecr:DeleteRepository",
-          "ecr:DeleteRepositoryPolicy",
-          "ecr:DescribeImages",
-          "ecr:GetRepositoryPolicy",
-          "ecr:ListTagsForResource",
-          "ecr:TagResource",
-          "ecr:UntagResource",
-          "ecr:SetRepositoryPolicy",
-          "ecr:DeleteRepositoryPolicy",
-          "ecr:DescribeImageScanFindings",
-          "ecr:StartImageScan",
-          "ecr:StartLifecyclePolicyPreview",
-          "ecr:DeleteLifecyclePolicy",
-          "ecr:GetLifecyclePolicy",
-          "ecr:PutLifecyclePolicy",
-          "ecr:DescribePullThroughCacheRules",
-          "ecr:CreatePullThroughCacheRule",
-          "ecr:DeletePullThroughCacheRule",
-          "ecr:TagResource",
-          "ecr:UntagResource",
-          "ecr:DescribeReplicationConfigurations",
-          "ecr:PutReplicationConfiguration",
-          "ecr:GetAuthorizationToken",
-          "ecs:CreateCluster",
-          "ecs:DeregisterContainerInstance",
-          "ecs:DiscoverPollEndpoint",
-          "ecs:Poll",
-          "ecs:RegisterContainerInstance",
-          "ecs:StartTelemetrySession",
-          "ecs:UpdateContainerInstancesState",
-          "ecs:Submit*",
+          "ecr:*",
+          "ecs:*",
+          "s3:*",
+          "eks:*",
+          "lambda:*",
           "logs:CreateLogStream",
           "logs:PutLogEvents",
-          "s3:ListBucket",
-          "s3:GetObject",
-          "s3:PutObject",
           "logs:CreateLogGroup",
           "logs:CreateLogStream",
           "logs:PutLogEvents"
