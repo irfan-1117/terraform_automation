0ff52d78128b98d99632ea487169be76e2bb3079
Irfan Shaikh <irfan.shaikh@ibm.com>
2024-07-29 17:09:27 +0530

    adding python pip and awc cli installation in shell sript

--- instance.tf
+++ instance.tf
@@ -38,7 +38,7 @@ resource "aws_key_pair" "ssh_key" {
 resource "aws_instance" "webserver" {
   ami                  = var.AMIS[var.AWS_REGION]
   instance_type        = var.instance_type
-  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
+  iam_instance_profile = aws_iam_instance_profile.ecr_instance_profile.name
 
   # the security group
   vpc_security_group_ids = [aws_security_group.allow-ssh.id, aws_security_group.webserver-sg.id]
